9539c2592356a1fe93bbe3bb6b38608676f6dd96
Alf <alf.g.jr@gmail.com>
2019-12-25 21:36:19 -0800

    web: update status page with api/db/redis/worker health-checks.

--- web/src/components/Status.vue
+++ web/src/components/Status.vue
@@ -16,6 +16,28 @@
           </b-card>
       </div>
     </b-card-group>
+    <hr />
+
+    <b-card-group deck>
+      <div
+        v-for="(o, i) in health"
+        v-bind:key="i"
+        class="col-lg-3 col-md-3 p-1 mb-2"
+      >
+          <b-card
+            :header="i"
+            class="text-center"
+            style=""
+            >
+            <b-card-text>
+              <b-alert
+                show
+                :variant="['NOTOK', 0].includes(o) ? 'danger' : 'success'">{{ o }}</b-alert>
+            </b-card-text>
+          </b-card>
+      </div>
+    </b-card-group>
+
     <h2 class="text-center" v-if="!stats.jobs">No Stats Found</h2>
   </div>
 </template>
@@ -24,7 +46,10 @@
 import auth from '../auth';
 
 const UPDATE_INTERVAL = 5000;
+const HEALTH_UPDATE_INTERVAL = 5000;
+
 let intervalId;
+let healthIntervalId;
 
 export default {
   name: 'status',
@@ -32,16 +57,21 @@ export default {
   data() {
     return {
       stats: {},
+      health: {},
     };
   },
 
   mounted() {
     this.getStats();
+    this.getHealth();
+
     intervalId = setInterval(this.getStats, UPDATE_INTERVAL);
+    healthIntervalId = setInterval(this.getHealth, HEALTH_UPDATE_INTERVAL);
   },
 
   destroyed() {
     clearInterval(intervalId);
+    clearInterval(healthIntervalId);
   },
 
   methods: {
@@ -73,6 +103,26 @@ export default {
         })
         .catch((err) => {
           console.log(err);
+        });
+    },
+
+    getHealth() {
+      const url = '/api/health';
+
+      this.$http.get(url, {})
+        .then(response => (
+          response.json()
+        ))
+        .then((json) => {
+          this.health = json;
+        })
+        .catch(() => {
+          this.health = {
+            API: 'NOTOK',
+            DB: 'NOTOK',
+            Redis: 'NOTOK',
+            Workers: 0,
+          };
         });
     },
   },
