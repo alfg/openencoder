8d81238af8ee6b39a5014c5a99a89c1963e3c784
Alf <alf.g.jr@gmail.com>
2020-08-02 20:24:44 -0700

    sql: update settings_options and settings_defaults to support ftp settings.

--- scripts/21_settings_defaults.sql
+++ scripts/21_settings_defaults.sql
@@ -4,5 +4,6 @@ INSERT INTO public.settings (id, value, settings_option_id, encrypted) VALUES (3
 INSERT INTO public.settings (id, value, settings_option_id, encrypted) VALUES (4, 'inbound', 5, false);
 INSERT INTO public.settings (id, value, settings_option_id, encrypted) VALUES (5, 'sfo2', 7, false);
 INSERT INTO public.settings (id, value, settings_option_id, encrypted) VALUES (6, 'disabled', 10, false);
+INSERT INTO public.settings (id, value, settings_option_id, encrypted) VALUES (7, 's3', 14, false);
 
 SELECT setval('settings_id_seq', max(id)) FROM settings;
